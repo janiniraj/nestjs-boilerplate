cdfa22ddff46faaa07a399897922320dca35c3c4
Brian Schulte <bschulte@Brians-iMac.local>
2019-01-10 20:41:02 -0500

    added config setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 5000;
+  Logger.error(`Listening on port: ${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
