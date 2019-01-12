cb4c65cb14b470ffc04f9f8e5e895dd051d452bc
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-12 00:30:55 -0500

    added create user and roles in resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,21 +1,46 @@
-import { Resolver, Query, Context } from '@nestjs/graphql';
-import { UseGuards, Req } from '@nestjs/common';
+import {
+  Resolver,
+  Query,
+  Context,
+  ResolveProperty,
+  Parent,
+  Mutation,
+  Args
+} from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { EmmLogger } from 'src/logger/logger';
-import { GqlAuthGuard } from 'src/auth/guards/graphqlAuth.guard';
+import { GqlAuthGuard } from 'src/common/guards/graphqlAuth.guard';
+import { RoleService } from 'src/role/role.service';
 import { User } from './user.entity';
-import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Resolver('User')
+@UseGuards(GqlAuthGuard)
 export class UserResolver {
   private readonly logger = new EmmLogger(UserResolver.name);
 
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly roleService: RoleService
+  ) {}
 
   @Query()
-  @UseGuards(GqlAuthGuard)
-  async user(@Context('req') req) {
-    return this.userService.findOneByEmail(req.user.email);
+  async user(@Context('req') { user }) {
+    return this.userService.findOneByEmail(user.email);
+  }
+
+  @Mutation()
+  async createUser(
+    @Args('email') email: string,
+    @Args('password') password: string
+  ) {
+    return this.userService.createUser({ email, password });
+  }
+
+  @ResolveProperty()
+  async roles(@Parent() user: User) {
+    return await this.roleService.findAll({ userId: user.id });
   }
 }
