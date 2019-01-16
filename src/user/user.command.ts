151d50e8ebdf2634dce32c86b382e07b36ef0f06
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-16 00:01:27 -0500

    create user command added

--- src/user/user.command.ts
+++ src/user/user.command.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Command, Positional } from 'nestjs-command';
+import { Command, Positional, Option } from 'nestjs-command';
+import { BackendLogger } from 'src/logger/BackendLogger';
+import { randomStr } from 'src/common/utils';
 
 @Injectable()
 export class UserCommand {
+  private readonly logger = new BackendLogger(UserCommand.name);
+
   constructor(private readonly userService: UserService) {}
 
   @Command({ command: 'user:create <email>', describe: 'create a new user' })
@@ -15,6 +19,10 @@ export class UserCommand {
     })
     email: string
   ) {
-    console.log('Creating new user: ', email);
+    this.logger.log(`Creating new user: ${email}`);
+    const password = randomStr(16);
+
+    await this.userService.createUser({ email, password });
+    this.logger.log(`Created, generated password: ${password}`);
   }
 }
